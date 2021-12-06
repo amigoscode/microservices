f380637d80b3ff5990d3465a50da4ab35e0684d7
amigoscode <amigoscode@gmail.com>
2021-12-06 13:57:31 +0000

    eureka

--- customer/src/main/java/com/amigoscode/customer/CustomerConfig.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerConfig.java
@@ -1,5 +1,6 @@
 package com.amigoscode.customer;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -20,9 +20,9 @@ public class CustomerService {
         // todo: check if email valid
         // todo: check if email not taken
         customerRepository.saveAndFlush(customer);
-        // todo: check if fraudster
+
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
